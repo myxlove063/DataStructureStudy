3d3c764f7c5733bad2fc3809e4a4cb9269c6aa67
myxleo <myxlove063@gmail.com>
2024-11-13 12:22:49 +0800

    ElegyNine

---
+++ 6.树和二叉树/6.5树与二叉树的应用/6.5.3堆/6.5.3堆.cpp
@@ -0,0 +1,29 @@
+//
+// Created by 24405 on 24-11-12.
+//
+
+//将一个数组调整成一个堆
+void adjustHeap(int a[], int k, int len) {
+    //对以k为根的子树进行调整
+    int tmp = a[k];
+    for (int i = 2 * k; i <= len; i *= 2) {
+        if (i < len && a[i] < a[i + 1]) {
+            i++;
+        }
+        if (tmp > a[i])break;
+        else {
+            a[k] = a[i];
+            k = i;
+        }
+    }
+    a[k] = tmp;
+}
+
+void BuildMaxHeap(int a[], int len) {
+    for (int i = len / 2; i ; i--) {
+        adjustHeap(a, i, len);
+    }
+}
+
+
+
